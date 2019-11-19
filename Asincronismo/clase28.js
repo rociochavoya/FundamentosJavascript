b4961b9d8e8ceb49238346118acd0435783d13d9
RocioChavoya <rocio.chavoya@gmail.com>
2019-11-18 19:02:12 -0600

    get con jquery

--- Asincronismo/clase28.js
+++ Asincronismo/clase28.js
@@ -1,4 +1,15 @@
-setTimeout( () => console.log("d"), 2000)
-for (var i=0; i < 1000000000 ; i++) {
+// setTimeout( () => console.log("d"), 2000)
+// for (var i=0; i < 1000000000 ; i++) {
 
-}
+// }, 
+
+const API_URL = "https://swapi.co/api/";
+const PEOPLE_URL = "people/:id";
+const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`;
+const opts = { crossDomain: true};
+const fnc = function (person) {
+    console.log(`Hola yo soy, ${person.name}`)
+   }
+
+$.get(lukeUrl, opts, fnc )
+
