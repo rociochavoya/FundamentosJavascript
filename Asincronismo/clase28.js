806581ae362bc8caaa737ab5f743126dc013ccf1
RocioChavoya <rocio.chavoya@gmail.com>
2019-12-17 13:50:00 -0600

    clase29 callback hell

--- Asincronismo/clase28.js
+++ Asincronismo/clase28.js
@@ -6,17 +6,34 @@
 const API_URL = "https://swapi.co/api/";
 const PEOPLE_URL = "people/:id";
 const opts = { crossDomain: true};
-const fnc = function (person) {
-    console.log(`Hola yo soy, ${person.name}`)
-   }
 
-function obtenerPersonaje (id) {
+
+function obtenerPersonaje (id, callback) {
     const url =  `${API_URL}${PEOPLE_URL.replace(':id', id)}`
-    $.get(url, opts, fnc )
+    $.get(url, opts, function (person) {
+        console.log(`Hola yo soy, ${person.name}`)
+
+        if(callback) {
+            callback()
+        }
+       } )
 }
-for (let i=0; i<=100; i++){
-  console.log(obtenerPersonaje(i))  
-}
+
+obtenerPersonaje(1, function () {
+    obtenerPersonaje(2, function () {
+        obtenerPersonaje(3, function () {
+            obtenerPersonaje(4, function() {
+                obtenerPersonaje(5, function () {
+                    obtenerPersonaje(6, function () {
+                        obtenerPersonaje(7, function () {
+
+                        })
+                    })
+                })
+            })
+        })
+    })
+})
 
 
 
